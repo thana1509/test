b23dd42828ad43fb19c38ed76d6d37d77f69d0db
Nattapong Kangwarawut <poppap@Nattapongs-MacBook-Air.local>
2021-11-27 15:43:17 +0700

    edit que final1

--- api/calendar/get_calendar.js
+++ api/calendar/get_calendar.js
@@ -2,11 +2,12 @@ const db = require("../../config/database");
 
 const getCalendar = (req, res, next) => {
     const userId = req.query.userID
+    const date = req.query.date
     db.getConnection(function (err, connection) {
         if (err) {
             res.status(500).json({"status": "Error", "message": "Connection DB Error"})
         } else {
-            connection.query(`SELECT calendarName, date, time, location FROM calendar WHERE userId = '${userId}'`, function (error, result) {
+            connection.query(`SELECT calendarName, date, time, location FROM calendar WHERE userId = '${userId}' AND date = '${date}'`, function (error, result) {
                 connection.release()
                 if (error) {
                     res.status(500).json({"status": "Error", "message": `${error.message}`})
--- api/work/schedule/get_schedule.js
+++ api/work/schedule/get_schedule.js
@@ -1,12 +1,16 @@
 const db = require("../../../config/database");
 
 const getSchedule = (req, res, next) => {
-    const date = req.query.date
+    // const date = req.query.date
+    const userID = req.query.userID
+
     db.getConnection(function (err, connection) {
         if (err) {
             res.status(500).json({"status": "Error", "message": "Connection DB Error"})
         } else {//TODO edit query
-            connection.query(`SELECT maintenanceID, maintenanceDetail, location, CONCAT('http://localhost:3000/',image) AS image,  status, createDtm FROM maintenance WHERE userID = '001' `, function (error, result) {
+            connection.query(`SELECT time, scheduleName, status FROM schedule 
+                              INNER JOIN calendar on schedule.calendarID = calendar.calendarID  
+                              INNER JOIN user on user.userID = calendar.userID WHERE userID = '${userID}' `, function (error, result) {
                 connection.release()
                 if (error) {
                     res.status(500).json({"status": "Error", "message": `${error.message}`})
